6a2254d624f63dcf2dbf9ddd75d3c748e5750709
St0ny <42348051+Raz0r1337@users.noreply.github.com>
2025-06-24 20:03:07 +0200

    Update quest_party_acore_string.sql
    Added german localisation

--- data/sql/db-world/base/quest_party_acore_string.sql
+++ data/sql/db-world/base/quest_party_acore_string.sql
@@ -1,4 +1,4 @@
 SET @ENTRY:=35440;
 DELETE FROM `acore_string` WHERE `entry`=@ENTRY;
 INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES
-(@ENTRY, 'This server is running the |cff4CFF00Quest Loot Party |rmodule.', '', '', '', '', '', 'Este servidor está ejecutando el módulo |cff4CFF00Quest Loot Party.', 'Este servidor está ejecutando el módulo |cff4CFF00Quest Loot Party.', '');
+(@ENTRY, 'This server is running the |cff4CFF00Quest Loot Party |rmodule.', '', '', 'Dieser Server verwendet das |cff4CFF00Quest Loot Party |rModul.', '', '', 'Este servidor está ejecutando el módulo |cff4CFF00Quest Loot Party.', 'Este servidor está ejecutando el módulo |cff4CFF00Quest Loot Party.', '');
